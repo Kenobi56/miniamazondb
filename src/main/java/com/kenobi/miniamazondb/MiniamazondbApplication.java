7a24045cbe4b046ffe8dbc8526c51a50fe6724cb
Steven MAHE <s.mahe@cat-amania.com>
2024-11-08 10:27:49 +0100

    First commit

---
+++ src/main/java/com/kenobi/miniamazondb/MiniamazondbApplication.java
@@ -0,0 +1,13 @@
+package com.kenobi.miniamazondb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniamazondbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniamazondbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenobi/miniamazondb/MiniamazondbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenobi.miniamazondb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniamazondbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
